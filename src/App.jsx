c4d2e6faf000c46b140d19865ffc0eb9e9b18a50
aitorortega03 <aitorortega21@gmail.com>
2024-03-07 23:36:59 +0100

    add pico.css by cdn and done main ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import { IconContext } from "react-icons";
 import { FaComputer } from "react-icons/fa6";
 
 const App = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("Submit IP: ", event.target[0].value)
+  }
+
   return (
-    <>
-      <IconContext.Provider value={{ size: "100px", className: "global-class-name" }} >
-        <div>
-          <FaComputer />
-        </div>
-      </IconContext.Provider>
-    </>
+    <main className="container">
+      <div style={{display: "grid", placeItems: "center"}}>
+        <IconContext.Provider value={{ size: "100px", className: "global-class-name" }}>
+          <div>
+            <FaComputer />
+          </div>
+        </IconContext.Provider>
+        <h1>IP Information Browser</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+      <label>
+        User IP:
+        <input type="text" placeholder="Introduce here the IP"/>
+        <small>For example: 24.48.0.1</small>
+        <button style={{width: "100%"}}>Search IP information</button>
+      </label>
+      </form>
+
+      <div>
+        <pre></pre>
+      </div>
+    </main>
   );
 };
 
