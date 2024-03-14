cd1162d6aed0b9487a1b997dd4030e0f254eb43c
aitorortega03 <aitorortega21@gmail.com>
2024-03-14 20:06:59 +0100

    end tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,9 @@
-import { describe, it, expect} from 'vitest';
-import { render, screen } from '@testing-library/react';
+import axios from "axios";
+import { describe, it, expect, afterEach } from 'vitest';
+import { cleanup, render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+
+afterEach(() => cleanup())
 
 describe('<App />', () => {
     it("should render App component", () => {
@@ -11,7 +14,13 @@ describe('<App />', () => {
     it("should initialize state variables to null or false when called", () => {
       render(<App />);
       const ipElement = screen.getByPlaceholderText("Introduce here the IP");
-
       expect(ipElement.value).toBe("");
+    });
+
+    it('should display the Header, Form, and Info components when rendered', () => {
+      render(<App />);
+      expect(screen.getByText('IP Information Browser')).toBeInTheDocument();
+      expect(screen.getByText('User IP:')).toBeInTheDocument();
+      expect(screen.getByText('Search IP information')).toBeInTheDocument();
     });
 })
